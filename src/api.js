88a7864d4ab25720b1b9f3b09431b3de95393c9c
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-09-18 14:45:31 -0400

    set up

---
+++ src/api.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const router = express.Router()
+
+module.exports.handler = serverless(app)
+
+router.get('/', async( req, res, next )=> {
+  console.log('hello world')
+  res.json({
+    "hello": "world"
+  })
+})
+
+app.use('/.netlify/functions/api', router)
